4db2e36b56ae96ea4518f5c409a6623187740f53
Nicholas McCollum <nimccoll@microsoft.com>
2023-03-13 11:42:23 -0400

    Separated Load Balancer ARM template from infrastructure ARM temp

--- PowerShell/infrastructure-deploy.ps1
+++ PowerShell/infrastructure-deploy.ps1
@@ -5,7 +5,13 @@ az login
 az group create --name nimccfta-vmss-bluegreen-rg --location eastus
 
 # Validate ARM template for infrastructure
-az deployment group validate -g nimccfta-vmss-bluegreen-rg --template-file infrastructure.json --parameters infrastructure.parameters.json --mode Incremental
+az deployment group validate -g nimccfta-vmss-bluegreen-rg --template-file infrastructure-no-lb.json --parameters infrastructure.parameters.json --mode Incremental
 
 # Deploy ARM template for infrastructure
-az deployment group create -g nimccfta-vmss-bluegreen-rg --template-file infrastructure.json --parameters infrastructure.parameters.json --mode Incremental
+az deployment group create -g nimccfta-vmss-bluegreen-rg --template-file infrastructure-no-lb.json --parameters infrastructure.parameters.json --mode Incremental
+
+# Validate ARM template for infrastructure
+az deployment group validate -g nimccfta-vmss-bluegreen-rg --template-file loadbalancer.json --parameters loadbalancer.parameters.json --mode Incremental
+
+# Deploy ARM template for infrastructure
+az deployment group create -g nimccfta-vmss-bluegreen-rg --template-file loadbalancer.json --parameters loadbalancer.parameters.json --mode Incremental
---
+++ PowerShell/swap.ps1
@@ -0,0 +1,8 @@
+﻿# Login to Azure
+az login
+
+# Validate ARM template for backend pool swap
+az deployment group validate -g nimccfta-vmss-bluegreen-rg --template-file loadbalancer.json --parameters loadbalancer.parameters.json --mode Incremental
+
+# Deploy ARM template for backend pool swap
+az deployment group create -g nimccfta-vmss-bluegreen-rg --template-file loadbalancer.json --parameters loadbalancer.parameters.json --mode Incremental
